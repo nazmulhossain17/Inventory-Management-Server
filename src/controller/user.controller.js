5398d14cf95ae490dd7a9188a0dc80d2d6873409
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-12 10:40:59 +0600

    token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -27,13 +27,14 @@ const registerUser = asyncHandler(async (req, res) => {
         return;
     }
 
-   
+    
 
     const user = await User.create({ name, email, password });
+    const token = generateToken(user._id)
 
     if (user) {
         const { _id, name, email, photo, phone, bio } = user;
-        res.status(201).json({ _id, name, email, photo, phone, bio });
+        res.status(201).json({ _id, name, email, photo, phone, bio, token });
     } else {
         res.status(400).json({ error: 'User registration failed' });
     }
