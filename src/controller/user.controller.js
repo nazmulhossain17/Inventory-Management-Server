7b7bca8f8c9e9639aeac7b9ec72dc9baf5509f66
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-12 10:21:13 +0600

    model next

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,4 @@
 const asyncHandler = require('express-async-handler');
-const bcrypt = require("bcryptjs");
 const User = require('../models/user.model');
 
 
@@ -22,10 +21,9 @@ const registerUser = asyncHandler(async (req, res) => {
         return;
     }
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+   
 
-    const user = await User.create({ name, email, password: hashedPassword });
+    const user = await User.create({ name, email, password });
 
     if (user) {
         const { _id, name, email, photo, phone, bio } = user;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -37,6 +37,16 @@ const userSchema = mongoose.Schema({
     timestamps: true,
 });
 
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+})
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
